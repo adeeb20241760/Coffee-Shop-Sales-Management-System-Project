f7993542d5779155aa230e0e8032d296ecb68815
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-03 09:11:07 +0530

    Extracted coffee sales data and stored in a dataframe

---
+++ etl.py
@@ -0,0 +1,4 @@
+# Extarcting data from excel file
+import pandas as pd
+coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Management-System-Project\\Coffee Shop Sales.xlsx')
+
