87a3ba2004ca2e4d7c02b2d69d13f352b5a8bb16
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-04 11:29:48 +0530

    feat: add negative values check
    To catch anomalies. Its not possible to have negative prices or quantities. In addition, having negative IDs do not make sense

--- etl.py
+++ etl.py
@@ -7,3 +7,14 @@ coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Manag
 # Null Values
 print(coffee_sales.isnull().sum())
 
+# Negative values 
+negative_qty = (coffee_sales['transaction_qty'] < 0).any()
+negative_price = (coffee_sales['unit_price'] < 0).any()
+negative_transaction_id = (coffee_sales['transaction_id'] < 0).any()
+negative_store_id = (coffee_sales['store_id'] < 0).any()
+negative_product_id = (coffee_sales['product_id'] < 0).any()
+print(f"Negative values in 'transaction_qty'? {negative_qty}")
+print(f"Negative values in 'unit_price'? {negative_price}") 
+print(f"Negative values in 'transaction_id'? {negative_transaction_id}")
+print(f"Negative values in 'store_id'? {negative_store_id}")
+print(f"Negative values in 'product_id'? {negative_product_id}")
