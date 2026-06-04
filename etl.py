7f65684eb732abeee63faee85dbdbc33c83b3f16
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-04 11:20:36 +0530

    feat: add null values check

--- etl.py
+++ etl.py
@@ -2,3 +2,8 @@
 import pandas as pd
 coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Management-System-Project\\Coffee Shop Sales.xlsx')
 
+# Transforming data
+
+# Null Values
+print(coffee_sales.isnull().sum())
+
