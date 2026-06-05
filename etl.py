6a4d5f68304fe23aabaf88e760d138b3a64d3792
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-05 17:48:01 +0530

    refactor: relocate EDA of dataset from etl.py to data_investigation.ipynb
    It is more appropriate to have such codes in the latter file. 'etl.py' should be a script that actively deals with data cleaning issues rather than just highlight it.

--- etl.py
+++ etl.py
@@ -4,21 +4,3 @@ coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Manag
 
 # Transforming data
 
-# Null Values
-print(coffee_sales.isnull().sum())
-
-# Negative values 
-negative_qty = (coffee_sales['transaction_qty'] < 0).any()
-negative_price = (coffee_sales['unit_price'] < 0).any()
-negative_transaction_id = (coffee_sales['transaction_id'] < 0).any()
-negative_store_id = (coffee_sales['store_id'] < 0).any()
-negative_product_id = (coffee_sales['product_id'] < 0).any()
-print(f"Negative values in 'transaction_qty'? {negative_qty}")
-print(f"Negative values in 'unit_price'? {negative_price}") 
-print(f"Negative values in 'transaction_id'? {negative_transaction_id}")
-print(f"Negative values in 'store_id'? {negative_store_id}")
-print(f"Negative values in 'product_id'? {negative_product_id}")
-
-# Duplicate values
-duplicate_rows = coffee_sales.duplicated().sum()
-print(f"Number of duplicate rows: {duplicate_rows}")
