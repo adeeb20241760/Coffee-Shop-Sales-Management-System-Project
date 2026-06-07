3c5e8e0f129ef73dd998d649cc2c1a5394a450fa
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-07 07:25:51 +0530

    feat: add null value remover

--- etl.py
+++ etl.py
@@ -4,3 +4,14 @@ coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Manag
 
 # Transforming data
 
+## Removing null values
+from numpy import shape
+initial_row_count = shape(coffee_sales)[0] 
+coffee_sales = coffee_sales.dropna()
+null_values_removed = initial_row_count - shape(coffee_sales)[0]
+
+if null_values_removed > 0:
+    print(f"Removed {null_values_removed} rows with null values.")
+else:
+    print("No null values found in the dataset.")    
+
