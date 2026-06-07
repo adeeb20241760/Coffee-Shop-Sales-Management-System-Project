db3209dc39412db90f525410b759ab9a0a9d688f
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-06-07 08:52:21 +0530

    fix: add relative path during extraction
    Hardcoding 'etl.py' with the absolute path will cuase the script to crash when run by another user if they clone the repository

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
 # Extarcting data from excel file
 import pandas as pd
-coffee_sales = pd.read_excel('C:\\Users\\Adeeb\\Desktop\\Coffee-Shop-Sales-Management-System-Project\\Coffee Shop Sales.xlsx')
+coffee_sales = pd.read_excel('Coffee Shop Sales.xlsx')
 
 # Transforming data
 
